f40e5694ac70754bd400e7deeed9312afb295d41
dtm <mercedes1993@bupt.edu.cn>
2019-02-19 10:44:24 +0800

    2019-02-19-10:44:24

--- Aizu_ALDS1_12_B/main.cpp
+++ Aizu_ALDS1_12_B/main.cpp
@@ -38,22 +38,25 @@ int main() {
 	int uId = 0;
 	v[uId] = 1;
 	
-	for(int i = 0; i < numv - 1; i++) {
-		int minl = 100001;
+	while(1) {
+		int minl = CMAX;
 		int newUId = -1;
 		for(int j = 0; j < numv; j++) {
 			if (!v[j]){
-				l[j] = minOf(l[j], l[uId] + table[uId][j]);
+				if(table[uId][j] < CMAX) {
+					l[j] = minOf(l[j], l[uId] + table[uId][j]);
+				}
 				if(l[j] <= minl) {
 					newUId = j;
 					minl = l[j];
 				}
 			}
 		}
-		
+		if(newUId == -1) {
+			break;
+		}
 		uId = newUId;
 		v[uId] = 1;
-	
 	}
 	for(int i = 0; i < numv; i++) {
 		cout << i << " "  << l[i] << endl;
